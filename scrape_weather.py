550ea4bdb1a068be014dd9699ed978479973a182
chris <chrispod47@gmail.com>
2020-03-31 11:14:44 -0500

    changed the output temporarily for debugging

--- scrape_weather.py
+++ scrape_weather.py
@@ -75,10 +75,11 @@ class MyHTMLParser(HTMLParser):
         self.nested_data = {}
 
 myparser = MyHTMLParser()
-with urllib.request.urlopen('https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=2018&EndYear=2018&Day=1&Year=2018&Month=1') as response:
+with urllib.request.urlopen('https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=2018&EndYear=2018&Day=1&Year=2018&Month=2') as response:
   res = str(response.read().decode())
   myparser.feed(res)
   myparser.data.popitem()
 
-for k, v in myparser.data.items():
-  print(k + ': ' + str(v))
+# for k, v in myparser.data.items():
+#   print(k + ': ' + str(v))
+print(str(myparser.data))
